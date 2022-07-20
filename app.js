a214ff8d05fa4af93ebbbb0abde1b3057372e12a
0xelia <elia.vanon@yahoo.com>
2022-07-20 22:10:02 +0200

    update

--- app.js
+++ app.js
@@ -13,25 +13,23 @@ console.log(km)
 let = age = parseInt(prompt('Quanti anni hai?'))
 console.log(age)
 
-if (isNaN(km, age)) {
+if ((isNaN(km)) || (isNaN(age))) {
     console.log('perfavore, inserire il numero di chilometri che si desidera percorrere')
 }
 
 // Calcolare prezzo base
 let floorPrice = (km * 0.21.toFixed(2))
-console.log('Floor price 'floorPrice)
+console.log('Floor price ' + floorPrice)
 
 // SE il passeggiero ha un'età < di 18 
 if (age < 18){
     // sconto 0.2
     salePrice = floorPrice - (floorPrice * 0.20)
-    console.log('Prezzo scontato under 18 ' + floorPrice + '€') 
+    console.log('Prezzo scontato under 18 ' + salePrice + '€') 
 // ALTRIMENTI SE ha un'età > di 65
 } else if (age > 65) {
     // sconto 0.4
-    floorPrice = km - (km * 0.40)
-    console.log('Prezzo scontato over 65 ' + floorPrice + '€')
+    salePrice = floorPrice - (floorPrice * 0.40)
+    console.log('Prezzo scontato over 65 ' + salePrice + '€')
 }
-    
-
     
