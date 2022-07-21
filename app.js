cbc78f365cf450ffb4b41cacdce4eaf2ce294260
0xelia <elia.vanon@yahoo.com>
2022-07-21 13:55:33 +0200

    js fixed

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
-// let random = (Math.random().toFixed(2));
-// console.log(random)
-
 //DATI CERTI
 const pPerKm = 0.21
 const saleUnder18 = 0.20
 const saleOver65 = 0.40
+let sale = 0
 
 // Chiedere all' utente quanti chilometri vuole fare e l'età del passeggero
 let km = prompt('Quanti chilometri vuoi fare?')
 km = parseInt(km)
-console.log(km)
-let = age = parseInt(prompt('Quanti anni hai?'))
-console.log(age)
+
+let age = parseInt(prompt('Quanti anni hai?'))
 
 if ((isNaN(km)) || (isNaN(age))) {
-    console.log('perfavore, inserire il numero di chilometri che si desidera percorrere')
+    alert('perfavore, inserire il numero di chilometri che si desidera percorrere')
 }
 
 // Calcolare prezzo base
-let floorPrice = (km * 0.21.toFixed(2))
-console.log('Floor price ' + floorPrice)
+
+let floorPrice = (km * pPerKm)
 
 // SE il passeggiero ha un'età < di 18 
 if (age < 18){
     // sconto 0.2
-    salePrice = floorPrice - (floorPrice * 0.20)
-    console.log('Prezzo scontato under 18 ' + salePrice + '€') 
+    sale = floorPrice * saleUnder18
+    let saleElement = document.getElementById('sale')
+    saleElement.innerHTML = 'Sconto: ' + sale.toFixed(2) + '€'
+
 // ALTRIMENTI SE ha un'età > di 65
 } else if (age > 65) {
     // sconto 0.4
-    salePrice = floorPrice - (floorPrice * 0.40)
-    console.log('Prezzo scontato over 65 ' + salePrice + '€')
-}
+    sale = floorPrice * saleOver65
+    saleElement = document.getElementById('sale')
+    saleElement.innerHTML = 'Sconto: ' + sale.toFixed(2) + '€'}
     
+
+let price = floorPrice - sale
+let salePriceElement = document.getElementById('price_sale')
+salePriceElement.innerHTML = 'Prezzo: ' + (price.toFixed(2)) + '€';
