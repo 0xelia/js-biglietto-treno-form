adcb1697ab4f9418b3da8470ea1ce93eba0f8392
0xelia <elia.vanon@yahoo.com>
2022-07-21 17:29:31 +0200

    JS update

--- app.js
+++ app.js
@@ -3,36 +3,49 @@ const pPerKm = 0.21
 const saleUnder18 = 0.20
 const saleOver65 = 0.40
 let sale = 0
+let km = 0
+let floorPrice = 0
+let age = 20
+let name = 'Username'
+// Chiedere all' utente il nome, quanti chilometri vuole fare e l'età del passeggero
 
-// Chiedere all' utente quanti chilometri vuole fare e l'età del passeggero
-let km = prompt('Quanti chilometri vuoi fare?')
-km = parseInt(km)
+name = document.querySelector('input[name="username"]')
+name = name.value
+console.log(name)
 
-let age = parseInt(prompt('Quanti anni hai?'))
+// if ((isNaN(km)) || (isNaN(age))) {
+//     alert('perfavore, inserire il numero di chilometri che si desidera percorrere')
+// }
 
-if ((isNaN(km)) || (isNaN(age))) {
-    alert('perfavore, inserire il numero di chilometri che si desidera percorrere')
-}
+// // Calcolare prezzo base quanto preme invia
 
-// Calcolare prezzo base
+let buttonElement = document.getElementById('button')
+buttonElement.addEventListener( 'click', 
+function() {
 
-let floorPrice = (km * pPerKm)
+    name = document.querySelector('input[name="username"]')
+    name = name.value
+    console.log('Username: ' + name)
 
-// SE il passeggiero ha un'età < di 18 
-if (age < 18){
-    // sconto 0.2
-    sale = floorPrice * saleUnder18
-    let saleElement = document.getElementById('sale')
-    saleElement.innerHTML = 'Sconto: ' + sale.toFixed(2) + '€'
+    age = document.querySelector('select[name="age"]')
+    age = age.value
+    console.log('Sconto: ' + age)
 
-// ALTRIMENTI SE ha un'età > di 65
-} else if (age > 65) {
-    // sconto 0.4
-    sale = floorPrice * saleOver65
-    saleElement = document.getElementById('sale')
-    saleElement.innerHTML = 'Sconto: ' + sale.toFixed(2) + '€'}
+    km = document.querySelector('input[name="kms"]')
+    km = km.value
+    floorPrice = (km * pPerKm)
+    console.log('Prezzo base: ' + floorPrice)
+
+    if (age == 0.2) {
+
+        sale = floorPrice * saleUnder18
+    } else if (age == 0.4) {
+
+        sale = floorPrice * saleOver65}
     
+    let finalPrice = floorPrice - sale
+    console.log('Prezzo finale: ' + finalPrice) 
 
-let price = floorPrice - sale
-let salePriceElement = document.getElementById('price_sale')
-salePriceElement.innerHTML = 'Prezzo: ' + (price.toFixed(2)) + '€';
+} )
+
+
